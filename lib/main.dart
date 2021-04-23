46356fe7c69fb99b69748f6473563b9ac227bbc8
Danil Garmanov <danil.garmanov@protonmail.com>
2021-04-23 09:31:24 +0300

    A few renamings

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Untitled Vegan App Web Admin',
+      title: 'Plante Web Admin',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
